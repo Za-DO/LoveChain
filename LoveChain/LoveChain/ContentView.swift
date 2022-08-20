d278e4b298e9241c8e61ffbb1f1f67bd44d9ceff
Thina <frog7134@gmail.com>
2022-08-20 20:12:22 +0900

    [feat] project 파일 생성

---
+++ LoveChain/LoveChain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoveChain
+//
+//  Created by 유정인 on 2022/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoveChain/LoveChain/LoveChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveChainApp.swift
+//  LoveChain
+//
+//  Created by 유정인 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct LoveChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
